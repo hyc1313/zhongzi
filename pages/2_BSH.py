5be8ba9ddf5156b2969ed8ee3a210b455cec7332
hyc1313 <hanyongchen00@gmail.com>
2026-06-10 15:48:25 +0800

    Add files via upload

---
+++ pages/2_BSH.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import joblib 
+
+model = joblib.load('D:/MLModels/BSH_xgboost_optimized_model.joblib')
+
+st.title("白沙蒿种子预测模型")
+
+feature_names = [f"特征{i+1}" for i in range(15)]  
+
+user_inputs = []  
+cols = st.columns(3)
+
+for i, name in enumerate(feature_names):
+    col = cols[i % 3]
+    value = col.number_input(
+        label=name,
+        value=0.0,
+        key=f"input_{i}"   
+    )
+    user_inputs.append(value)
+
+if st.button("开始预测"):
+    input_data = [user_inputs]   # shape (1,15)
+    prediction = model.predict(input_data)[0]  
+    st.success(f"预测结果是：{prediction}")
